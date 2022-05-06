f4a6a074d85453d435cebb1bba0a8a146e6fb16d
Colehubert <Colehubert2001@gmail.com>
2022-05-06 15:08:19 -0700

    adding some questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,35 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const  generateMarkdown  = require('./utils/generateMarkdown');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+type: 'input',
+name: 'firstName',
+message: 'what is your first name?',
+},
+{
+type: 'input',
+name: 'lastName',
+message: 'what is your last name?',
+}];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((data)=> {
+      const markdown =   generateMarkdown(data);
+        fs.writeFile('README.MD',markdown, (err)=>{
+            if(err){
+                console.log(err)
+            }
+            console.log("README file generated");
+        })
+    });
+}
 
 // Function call to initialize app
 init();
+
