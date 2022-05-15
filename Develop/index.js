6280fef2028194eb92e6321a38694623b6dd9c96
Colehubert <Colehubert2001@gmail.com>
2022-05-15 16:45:17 -0700

    adding more inputs

--- Develop/index.js
+++ Develop/index.js
@@ -5,14 +5,45 @@ const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [{
 type: 'input',
-name: 'firstName',
-message: 'what is your first name?',
+name: 'title',
+message: 'what is the name of your project?',
 },
 {
 type: 'input',
-name: 'lastName',
-message: 'what is your last name?',
-}];
+name: 'description',
+message: 'what would you describe your project as?',
+},
+{
+    type: 'input',
+    name: 'instructions',
+    message: 'what are your installation instructions?',
+    },
+    {
+        type: 'input',
+        name: 'design',
+        message: 'How was your project designed to be used?',
+        },
+        {
+            type: 'input',
+            name: 'contribute',
+            message: 'How can one contribute to your project?',
+            },
+            {
+                type: 'input',
+                name: 'license',
+                message: 'Provide License information.',
+                },
+                {
+                    type: 'input',
+                    name: 'username',
+                    message: 'Enter your GitHub Username.',
+                    },
+                    {
+                        type: 'input',
+                        name: 'email',
+                        message: 'Enter your email',
+                        }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
